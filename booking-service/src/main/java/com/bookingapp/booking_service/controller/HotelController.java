28c3ee25947b9729e199ab0b4d90b6e7d1f0754f
prushorth <maniprush3@gmail.com>
2026-07-09 12:59:58 -0700

    Add hotel service

--- booking-service/src/main/java/com/bookingapp/booking_service/controller/HotelController.java
+++ booking-service/src/main/java/com/bookingapp/booking_service/controller/HotelController.java
@@ -1,5 +1,4 @@
 package com.bookingapp.booking_service.controller;
-
 import java.time.LocalDate;
 import java.util.List;
 
@@ -9,49 +8,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookingapp.booking_service.dto.HotelSearchResponse;
+import com.bookingapp.booking_service.service.HotelService;
 
 @RestController
+
 @CrossOrigin(origins = "http://localhost:5173")
+
 public class HotelController {
 
-    @GetMapping("/api/hotels/search")
-    public List<HotelSearchResponse> searchHotels(
-            @RequestParam String location,
-            @RequestParam LocalDate checkIn,
-            @RequestParam LocalDate checkOut,
-            @RequestParam(defaultValue = "1") int guests
-    ) {
-        return List.of(
-                new HotelSearchResponse(
-                        1L,
-                        "Downtown Seattle Hotel",
-                        "Seattle",
-                        "123 Pike Street",
-                        4.5,
-                        180,
-                        4,
-                        "https://images.unsplash.com/photo-1566073771259-6a8506099945"
-                ),
-                new HotelSearchResponse(
-                        2L,
-                        "Lake View Suites",
-                        "Seattle",
-                        "88 Lake Union Ave",
-                        4.7,
-                        230,
-                        2,
-                        "https://images.unsplash.com/photo-1551882547-ff40c63fe5fa"
-                ),
-                new HotelSearchResponse(
-                        3L,
-                        "Airport Business Inn",
-                        "Seattle",
-                        "500 Airport Way",
-                        4.1,
-                        140,
-                        6,
-                        "https://images.unsplash.com/photo-1564501049412-61c2a3083791"
-                )
-        );
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+
+        this.hotelService = hotelService;
+
     }
+
+    @GetMapping("/api/hotels/search")
+
+    public List<HotelSearchResponse> searchHotels(
+
+            @RequestParam String location,
+
+            @RequestParam LocalDate checkIn,
+
+            @RequestParam LocalDate checkOut,
+
+            @RequestParam(defaultValue = "1") int guests
+
+    ) {
+
+        return hotelService.searchHotels(location, checkIn, checkOut, guests);
+
+    }
+
 }
