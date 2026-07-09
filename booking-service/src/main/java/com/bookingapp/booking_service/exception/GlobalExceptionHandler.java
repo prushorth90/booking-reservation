b63dbfffd60c08ef25b544d6e3f44a4c33f688f9
prushorth <maniprush3@gmail.com>
2026-07-09 14:51:00 -0700

    Add exception handler to bokoing service

---
+++ booking-service/src/main/java/com/bookingapp/booking_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.bookingapp.booking_service.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+
+    public ResponseEntity<String> handleIllegalArgumentException(
+
+            IllegalArgumentException exception
+
+    ) {
+
+        return ResponseEntity
+
+                .status(HttpStatus.BAD_REQUEST)
+
+                .body(exception.getMessage());
+
+    }
+
+}
