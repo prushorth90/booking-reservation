1626fbdf04198eb14ba6bbc112c98a359e69e067
prushorth <maniprush3@gmail.com>
2026-07-21 13:09:04 -0700

    Add distributed tracing with open telemetry

--- booking-service/src/main/java/com/bookingapp/booking_service/config/AuthenticatedUserLoggingFilter.java
+++ booking-service/src/main/java/com/bookingapp/booking_service/config/AuthenticatedUserLoggingFilter.java
@@ -1,22 +1,16 @@
-import jakarta.servlet.FilterChain;
-
-import jakarta.servlet.ServletException;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-import jakarta.servlet.http.HttpServletResponse;
+package com.bookingapp.booking_service.config;
+import java.io.IOException;
 
 import org.slf4j.MDC;
-
 import org.springframework.security.core.Authentication;
-
 import org.springframework.security.core.context.SecurityContextHolder;
-
 import org.springframework.stereotype.Component;
-
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import java.io.IOException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 
